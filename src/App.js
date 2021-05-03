81f4fcf528c2cf2e787fb2caf9cd82734b5ea7c9
Todd Albert <todd@bocacode.com>
2021-05-03 16:16:01 -0400

    setup random beer selector

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [beers, setBeers] = useState(null)
+  const [randomBeer, setRandomBeer] = useState(null)
+  useEffect(() => {
+    fetch('https://beers-api-bc.web.app/beers')
+      .then(response => response.json())
+      .then(data => setBeers(data))
+      .catch(err => console.log(err))
+  }, [])
+  const getRandom = () => {
+    const elem = Math.floor(Math.random()*beers.length)
+    setRandomBeer(beers[elem].name)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Beers</h1>
+        <button onClick={() => getRandom()}>Pick a Beer</button>
+        {randomBeer && <h2>{randomBeer}<hr /></h2>}
+        {!beers ? <h2>Loading...</h2> : 
+          beers.map(beer => {
+            return <p key={beer.id}>{beer.name}</p>
+          })
+        }
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
